156fb6b4bceab8393c13e334e1081f8579b53672
Jonathan LI <Jonathanlyj@163.com>
2018-07-23 02:18:37 -0400

    first attempt with comments

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -11,13 +11,15 @@ function LCController($scope){
 //define check function triggered by cllick event
 $scope.check = function(){
   var lunchString = "".concat($scope.lunches);
+
+  //deal with the case of empty input
   if (lunchString == "" || lunchString == "undefined"){
     $scope.message = "Please enter data first" ;
   }
+  //deal with the case of having somthing enterd
   else{
     var arrayOfLunches  = lunchString.split(',');
     var numLunches = arrayOfLunches.length;
-
     if (numLunches <= 3){
       $scope.message = "Enjoy!" ;
     }
